14eaae4b24a0939a4cc42f66bd6c1484b391bcd8
AlexAlberoRibera <alberoriberaalex@gmail.com>
2026-02-09 15:03:58 +0100

    Finalizado

--- routes/api.php
+++ routes/api.php
@@ -13,38 +13,42 @@ use App\Http\Controllers\Api\PartidoController;
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 
-Route::get('jugadoras', [JugadoraController::class, 'index']);
-Route::get('jugadoras/{jugadora}', [JugadoraController::class, 'show']);
+Route::middleware('auth')->group(function () {
 
-Route::get('estadios', [EstadioController::class, 'index']);
-Route::get('estadios/{estadio}', [EstadioController::class, 'show']);
 
-Route::get('equipos', [EquipoController::class, 'index']);
-Route::get('equipos/{equipo}', [EquipoController::class, 'show']);
+    Route::get('jugadoras', [JugadoraController::class, 'index']);
+    Route::get('jugadoras/{jugadora}', [JugadoraController::class, 'show']);
 
-Route::get('partidos', [PartidoController::class, 'index']);
-Route::get('partidos/{partido}', [PartidoController::class, 'show']);
+    Route::get('estadios', [EstadioController::class, 'index']);
+    Route::get('estadios/{estadio}', [EstadioController::class, 'show']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('equipos', [EquipoController::class, 'index']);
+    Route::get('equipos/{equipo}', [EquipoController::class, 'show']);
 
-    Route::post('jugadoras', [JugadoraController::class, 'store']);
-    Route::put('jugadoras/{jugadora}', [JugadoraController::class, 'update']);
-    Route::delete('jugadoras/{jugadora}', [JugadoraController::class, 'destroy']);
+    Route::get('partidos', [PartidoController::class, 'index']);
+    Route::get('partidos/{partido}', [PartidoController::class, 'show']);
 
-    Route::post('estadios', [EstadioController::class, 'store']);
-    Route::put('estadios/{estadio}', [EstadioController::class, 'update']);
-    Route::delete('estadios/{estadio}', [EstadioController::class, 'destroy']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
 
-    Route::post('equipos', [EquipoController::class, 'store']);
-    Route::put('equipos/{equipo}', [EquipoController::class, 'update']);
-    Route::delete('equipos/{equipo}', [EquipoController::class, 'destroy']);
+        Route::post('jugadoras', [JugadoraController::class, 'store']);
+        Route::put('jugadoras/{jugadora}', [JugadoraController::class, 'update']);
+        Route::delete('jugadoras/{jugadora}', [JugadoraController::class, 'destroy']);
 
-     Route::post('partidos', [PartidoController::class, 'store']);
-    Route::put('partidos/{partido}', [PartidoController::class, 'updateResultado']);
-    Route::delete('partidos/{partido}', [PartidoController::class, 'destroy']);
+        Route::post('estadios', [EstadioController::class, 'store']);
+        Route::put('estadios/{estadio}', [EstadioController::class, 'update']);
+        Route::delete('estadios/{estadio}', [EstadioController::class, 'destroy']);
 
-    
+        Route::post('equipos', [EquipoController::class, 'store']);
+        Route::put('equipos/{equipo}', [EquipoController::class, 'update']);
+        Route::delete('equipos/{equipo}', [EquipoController::class, 'destroy']);
 
-    Route::get('user', [ProfileController::class, 'show']);
+        Route::post('partidos', [PartidoController::class, 'store']);
+        Route::put('partidos/{partido}', [PartidoController::class, 'updateResultado']);
+        Route::delete('partidos/{partido}', [PartidoController::class, 'destroy']);
+
+
+
+        Route::get('user', [ProfileController::class, 'show']);
+    });
 });
